3bbc67da11b2d836fba76cba11e096a4f9b62f99
Lucas Miranda <miranda.a.lucas@gmail.com>
2018-12-03 12:56:48 -0200

    Add tile mode to line tool

--- src/Tools/Tools.ts
+++ src/Tools/Tools.ts
@@ -5,6 +5,7 @@ import Color from '../Graphics/Color';
 import Graphics from '../Graphics/Graphics';
 import Draw from '../Draw';
 import Surface from '../Surface';
+import Point from '../Math/Point';
 import Line from '../Math/Line';
 import Settings from '../Settings';
 
@@ -58,13 +59,7 @@ export class PencilTool extends Tool {
                 if (this._isPlacing) {
                     Tool.bufferGraphicsContext.clear();
 
-                    Tool.bufferGraphicsContext.drawRectangle(
-                        pointerGridCell.x * Settings.gridCellSize.width,
-                        pointerGridCell.y * Settings.gridCellSize.height,
-                        Settings.gridCellSize.width,
-                        Settings.gridCellSize.height,
-                        { color: 0x292929, alpha: 1 }
-                    );
+                    fillCellAtGraphicsBuffer(pointerGridCell.x, pointerGridCell.y);
 
                     surface.draw(Tool.bufferGraphicsContext);
                 }
@@ -105,20 +100,73 @@ export class LineTool extends Tool {
     }
 
     public render(surface: Surface) {
-        // lines to render
-        Tool.bufferGraphicsContext.clear();
-        Tool.bufferGraphicsContext.lineStyle(0x292929);
-        while (this._linesToRender.length > 0) {
-            let line = this._linesToRender.splice(0, 1)[0];
-            Tool.bufferGraphicsContext.drawLine(line.start.x, line.start.y, line.end.x, line.end.y);
-        }
+        switch (Settings.targetMode) {
+            case TargetMode.Pixel:
+                // lines to render
+                Tool.bufferGraphicsContext.clear();
+                Tool.bufferGraphicsContext.lineStyle(0x292929);
+                while (this._linesToRender.length > 0) {
+                    let line = this._linesToRender.splice(0, 1)[0];
+                    Tool.bufferGraphicsContext.drawLine(line.start.x, line.start.y, line.end.x, line.end.y);
+                }
 
-        surface.draw(Tool.bufferGraphicsContext);
+                surface.draw(Tool.bufferGraphicsContext);
 
-        // current line
-        if (!this._isFirstPoint) {
-            Draw.lineStyle(0x292929);
-            Draw.line(this.currentLine.start.x, this.currentLine.start.y, this.currentLine.end.x, this.currentLine.end.y);
+                // current line
+                if (!this._isFirstPoint) {
+                    Draw.lineStyle(0x292929);
+                    Draw.line(this.currentLine.start.x, this.currentLine.start.y, this.currentLine.end.x, this.currentLine.end.y);
+                }
+                break;
+
+            case TargetMode.Tile:
+                // current line preview
+                if (!this._isFirstPoint) {
+                    let startCell = new Point(
+                        Math.floor(this.currentLine.start.x / Settings.gridCellSize.width),
+                        Math.floor(this.currentLine.start.y / Settings.gridCellSize.height)
+                    );
+
+                    let endCell = new Point(
+                        Math.floor(this.currentLine.end.x / Settings.gridCellSize.width),
+                        Math.floor(this.currentLine.end.y / Settings.gridCellSize.height)
+                    );
+
+                    fillCellAtPreview(startCell.x, startCell.y);
+                    fillCellAtPreview(endCell.x, endCell.y);
+                    CellLine(startCell, endCell, fillCellAtPreview);
+                }
+
+                // lines to render
+                if (this._linesToRender.length > 0) {
+                    Tool.bufferGraphicsContext.clear();
+                    //Tool.bufferGraphicsContext.lineStyle(0x292929);
+                    while (this._linesToRender.length > 0) {
+                        let line = this._linesToRender.splice(0, 1)[0];
+                        //Tool.bufferGraphicsContext.drawLine(line.start.x, line.start.y, line.end.x, line.end.y);
+
+                        let startCell = new Point(
+                            Math.floor(line.start.x / Settings.gridCellSize.width),
+                            Math.floor(line.start.y / Settings.gridCellSize.height)
+                        );
+
+                        let endCell = new Point(
+                            Math.floor(line.end.x / Settings.gridCellSize.width),
+                            Math.floor(line.end.y / Settings.gridCellSize.height)
+                        );
+
+                        fillCellAtGraphicsBuffer(startCell.x, startCell.y);
+                        fillCellAtGraphicsBuffer(endCell.x, endCell.y);
+                        CellLine(startCell, endCell, fillCellAtGraphicsBuffer);
+                    }
+
+                    surface.draw(Tool.bufferGraphicsContext);
+                }
+
+                break;
+
+            default:
+                break;
         }
     }
 
@@ -232,4 +280,92 @@ export class EraserTool extends Tool {
     private onEraseEnd(): void {
         this.isErasing = false;
     }
+}
+
+function CellLine(cellStart: Point, cellEnd: Point, plot: Function) {
+    if (Math.abs(cellEnd.y - cellStart.y) < Math.abs(cellEnd.x - cellStart.x)) {
+        if (cellStart.x > cellEnd.x) {
+            plotLineLow(cellEnd, cellStart, plot);
+        } else {
+            plotLineLow(cellStart, cellEnd, plot);
+        }
+    } else {
+        if (cellStart.y > cellEnd.y) {
+            plotLineHigh(cellEnd, cellStart, plot);
+        } else {
+            plotLineHigh(cellStart, cellEnd, plot);
+        }
+    }
+}
+
+function plotLineLow(cellStart: Point, cellEnd: Point, plot: Function) {
+    let dx = cellEnd.x - cellStart.x;
+    let dy = cellEnd.y - cellStart.y;
+    let yi = 1;
+
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
+
+    let D = 2 * dy - dx;
+    let y = cellStart.y;
+
+    for (let x = cellStart.x; x < cellEnd.x; x++) {
+        plot(x, y);
+
+        if (D > 0) {
+            y += yi;
+            D -= 2 * dx;
+        }
+
+        D += 2 * dy;
+    }
+}
+
+function plotLineHigh(cellStart: Point, cellEnd: Point, plot: Function) {
+    let dx = cellEnd.x - cellStart.x;
+    let dy = cellEnd.y - cellStart.y;
+    let xi = 1;
+
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+
+    let D = 2 * dx - dy;
+    let x = cellStart.x;
+
+    for (let y = cellStart.y; y < cellEnd.y; y++) {
+        plot(x, y);
+
+        if (D > 0) {
+            x += xi;
+            D -= 2 * dy;
+        }
+
+        D += 2 * dx;
+    }
+}
+
+function fillCellAtPreview(x: number, y: number) {
+    Draw.rectangle(
+        x * Settings.gridCellSize.width,
+        y * Settings.gridCellSize.height,
+        Settings.gridCellSize.width,
+        Settings.gridCellSize.height,
+        { color: 0x292929 },
+        null
+    );
+}
+
+function fillCellAtGraphicsBuffer(x: number, y: number) {
+    Tool.bufferGraphicsContext.drawRectangle(
+        x * Settings.gridCellSize.width,
+        y * Settings.gridCellSize.height,
+        Settings.gridCellSize.width,
+        Settings.gridCellSize.height,
+        { color: 0x292929, alpha: 1 },
+        null
+    );
 }
